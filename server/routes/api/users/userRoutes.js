5a603134cf8ad4a7187d9b1a388d97c0d8324c67
Devtle <littlenip16@gmail.com>
2024-01-21 14:02:25 -0600

    update
    57. GET : การดึงข้อมูลมาแสดง #1

--- server/routes/api/users/userRoutes.js
+++ server/routes/api/users/userRoutes.js
@@ -7,8 +7,22 @@ var { User } = require("../../../models");
 
 // Fetch all users
 router.get('/', function(req, res, next) {
-  User.findAll({}).then(data => {
+  let { page = 1, limit = 5, sort = "id", order = "asc", ...filter }  = req.query;
+  page = parseInt(page);
+  limit = parseInt(limit);
+  let offset = (page - 1) * limit;
+
+
+  User.findAll({
+    where: filter,
+    offset,
+    limit: limit <= 0 ? undefined : limit,
+    order: [[sort, order]]
+  }).then(data => {
     res.send(data);
+  }).catch((err) => {
+    const{ original : { cloneDeep, sqlMessage }} = err;
+    res.status(400).send({ error : { name: code, message : sqlMessage}});
   })
 });
 
